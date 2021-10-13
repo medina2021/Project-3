a56a94c952f0a0b803a4a141f3f863f12625391a
Aldo Quintero <aldoq95@gmail.com>
2021-10-12 19:58:19 -0700

    index file added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
 function App() {
-return (
-<div> hi </div>
-  );
+  return <div> hi </div>;
 }
 
 export default App;
